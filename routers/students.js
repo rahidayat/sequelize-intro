e9918cbe4e18eed2b3d8f879627997c6b5dc43c8
rahid <hidayat.85@gmail.com>
2017-07-13 22:20:57 +0700

    sampai add students, edit & delete belum

--- routers/students.js
+++ routers/students.js
@@ -4,11 +4,25 @@ const model = require ('../models');
 
 router.get('/', (req, res) => {
   model.Students.findAll().then(row => {
-    res.send(row)
-    // res.render('teachers', {teacher_data: row})
+    // res.send(row)
+    res.render('students', {students_data: row})
   })
 })
 
+router.get('/add', (req, res) => {
+    res.render('add-students')
+})
+
+router.post('/add', (req, res) => {
+  model.Students.create({
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email: req.body.email,
+    createdAt: new Date(),
+    updatedAt: new Date()
+  }).then(row => {res.redirect('/students')})
+
+})
 
 
 
